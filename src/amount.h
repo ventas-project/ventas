2c85c9aa798997a6fe179597d1abee7e77338e69
jhkim <jhkim@etra.kr>
2017-08-29 14:20:57 +0900

    change max money

--- src/amount.h
+++ src/amount.h
@@ -29,8 +29,8 @@ extern const std::string CURRENCY_UNIT;
  * for the creation of coins out of thin air modification could lead to a fork.
  * */
 // static const CAmount MAX_MONEY = 84,000,000 * COIN;
-static const CAmount MAX_MONEY = 1400000000 * COIN;
-
+// static const CAmount MAX_MONEY = 1400000000 * COIN;
+static const CAmount MAX_MONEY = 2000000000 * COIN;//2,000,000,000
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 /**
