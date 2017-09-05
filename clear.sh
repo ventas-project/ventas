010542f090d948d556bed1a058d1eb4b216b8577
jhkim <jhkim@etra.kr>
2017-09-05 14:23:57 +0900

    aa

--- clear.sh
+++ clear.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+timestamp=$( date +%Y%m%d-%H%M%S )
+echo $timestamp
+cp data/wallet.dat ./wallet.dat.$timestamp
 rm -Rf ./data/*
 cp *.conf ./data/
+cp ./wallet.dat.$timestamp ./data/wallet.dat
