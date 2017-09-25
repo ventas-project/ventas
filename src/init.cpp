3da0934cc229ee960fdb9a28ed79176d0adbd6ae
ventas-dev <ventas.core.dev@gmail.com>
2017-09-25 12:13:25 +0900

    remove comment experimental

--- src/init.cpp
+++ src/init.cpp
@@ -523,7 +523,7 @@ std::string LicenseInfo()
                URL_SOURCE_CODE) +
            "\n" +
            "\n" +
-           _("This is experimental software.") + "\n" +
+        //    _("This is experimental software.") + "\n" +
            strprintf(_("Distributed under the MIT software license, see the accompanying file %s or %s"), "COPYING", "<https://opensource.org/licenses/MIT>") + "\n" +
            "\n" +
            strprintf(_("This product includes software developed by the OpenSSL Project for use in the OpenSSL Toolkit %s and cryptographic software written by Eric Young and UPnP software written by Thomas Bernard."), "<https://www.openssl.org>") +
